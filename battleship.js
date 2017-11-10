5db223b0ebbf0d2b4a7cb21de9e72fc76355190a
snayua <snayu.arora@sterlingpublishing.com.au>
2017-11-10 16:40:47 +1100

    Dont have to click Fire! Just hitting enter will now take the input

--- battleship.js
+++ battleship.js
@@ -92,6 +92,8 @@ function parseGuess(guess){
 function init(){
     var fireButton = document.getElementById("fireButton");
     fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
 }
 
 function handleFireButton(){
@@ -99,6 +101,14 @@ function handleFireButton(){
     var guess = guessInput.value;
     controller.processGuess(guess);
     guessInput.value = "";
+}
+
+function handleKeyPress(e){
+    var fireButton = document.getElementById("fireButton");
+    if(e.keyCode === 13){
+        fireButton.click();
+        return false;
+    }
 }
 
 window.onload = init;
